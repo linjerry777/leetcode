4c2c8495d1d493f02d1d0e322f1f0df2c7193325
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-17 13:52:55 +0800

    Time: 89 ms (55.98%), Space: 43.6 MB (94.60%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -8,11 +8,13 @@ var canConstruct = function(ransomNote, magazine) {
     let hashmap = {}
     for(let i = 0;i<magazine.length;i++){
         let char = magazine[i];
+        //如果hash表裏面有該值+1 沒有設為1
         hashmap[char] =  hashmap[char]  ?  hashmap[char] +1: 1
     }
     
     for(let i = 0;i<ransomNote.length;i++){
         let char = ransomNote[i];
+        //如果表裏面有該值--阿如果沒有返回false
         if(hashmap[char]){
              hashmap[char]--
         }else{
