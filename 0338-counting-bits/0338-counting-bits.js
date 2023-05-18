6785deed6bfc1380842e1f374926bcc5dddf2281
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-18 14:51:10 +0800

    Time: 76 ms (90.42%), Space: 47.2 MB (98.86%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,17 +3,22 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    //定義陣列一開始一定是0
-    let result=[0]
-    for(let i =1;i<=n;i++){
-        //判斷最後一位是不是1
-        if(i%2==1){
-            //是1  ex r[3] = r[2]+1
-            result[i]  = result[i-1] +1
-        }else{
-            //不是1 r[2] = r[1]
-           result[i]  = result[i/2]
-        }
+    // //定義陣列一開始一定是0
+    // let result=[0]
+    // for(let i =1;i<=n;i++){
+    //     //判斷最後一位是不是1
+    //     if(i%2==1){
+    //         //是1  ex r[3] = r[2]+1
+    //         result[i]  = result[i-1] +1
+    //     }else{
+    //         //不是1 r[2] = r[1]
+    //        result[i]  = result[i/2]
+    //     }
+    // }
+    // return result
+    const result = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        result[i] = result[i & (i - 1)] + 1;
     }
-    return result
+    return result;
 };
