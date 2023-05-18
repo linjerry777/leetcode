3b976f30e67b71f3140146d4cd24df04f1320f2e
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-18 14:02:23 +0800

    Time: 126 ms (70.84%), Space: 47.4 MB (54.54%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 function romanToInt(s) {
+    //定義哈希表
   const romanMap = {
     'I': 1,
     'V': 5,
@@ -13,6 +14,7 @@ function romanToInt(s) {
     'M': 1000
   };
   let result = 0;
+    //當前一個數字比現在這個小時套用 當前-2*(上一個)+result 
   for (let i = 0; i < s.length; i++) {
     if (i > 0 && romanMap[s[i]] > romanMap[s[i - 1]]) {
       result += romanMap[s[i]] - 2 * romanMap[s[i - 1]];
