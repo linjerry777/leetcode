9947edc1fc678cc7f6f354f133565a8a2cb3d2bf
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-18 10:28:14 +0800

    Time: 55 ms (69.89%), Space: 41.8 MB (49.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,14 +3,20 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    
+    //如果<=2階答案就是n
     if(n<=2)return n
+    //設定前一階跟前前一階的有幾種方法 1階1種 2階2種
     let pre1 =1
     let pre2 =2
+    //從第3階爬到n階
     for(let i =3;i<=n;i++){
+        //count=上階+上上階
         let count = pre1+pre2
+        //上上階 = 上階
         pre1 = pre2
+        //上階 = 這階
         pre2 = count
     }
+    //回傳這階
     return pre2
 };
