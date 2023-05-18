7dda6385a3ff116cba982cf935ec581beca0be84
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-18 13:46:00 +0800

    Time: 102 ms (41.13%), Space: 55 MB (39.98%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,13 +3,15 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let card = new Set()
-   for (let i = 0; i < nums.length; i++) {
-        if (card.has(nums[i])) {
-            return true;
-        } else {
-            card.add(nums[i]);
-        }
-    }
-    return false
+   //  let card = new Set()
+   // for (let i = 0; i < nums.length; i++) {
+   //      if (card.has(nums[i])) {
+   //          return true;
+   //      } else {
+   //          card.add(nums[i]);
+   //      }
+   //  }
+   //  return false
+    //es6 Set寫法 太秀了 Set它只包含唯一的元素 所以比較小就代表有重複
+    return new Set(nums).size < nums.length
 };
