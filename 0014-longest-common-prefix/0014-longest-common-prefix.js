a593a2395ce7d556b06c56788de670ba5b2b4809
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-19 14:09:34 +0800

    Time: 66 ms (41.02%), Space: 43 MB (27.92%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,9 +3,11 @@
  * @return {string}
  */
 function longestCommonPrefix(strs) {
-    //
+    //如果沒有返回''
     if(!strs.length) return ''
+    //res = 字串的第一個單字
     let res = strs[0]
+    //ch = flower、flow、flight
     for(ch of strs){
         for(let i =0;i<res.length;i++){
             if(ch[i] !== res[i]){
