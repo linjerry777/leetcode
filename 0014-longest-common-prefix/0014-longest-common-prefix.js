793b9b48538005a992b4629c01f88868bf6d05d3
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-19 13:57:47 +0800

    Time: 61 ms (64.99%), Space: 43 MB (30.32%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+    //
+    if(!strs.length) return ''
+    let res = strs[0]
+    for(ch of strs){
+        for(let i =0;i<res.length;i++){
+            if(ch[i] !== res[i]){
+                res = res.slice(0,i)
+                break
+            }
+        }
+    }
+    return res
+}
