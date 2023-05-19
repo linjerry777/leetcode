be98af8e88f65eff1eccd19458190da65f66c5c4
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-19 14:56:56 +0800

    Time: 53 ms (97.29%), Space: 43.3 MB (75.08%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,12 +3,20 @@
  * @return {number}
  */
  const singleNumber = function (nums) {
-       let a ={}
-       for(let i = 0;i<nums.length;i++){
-           a[nums[i]] = a[nums[i]]?a[nums[i]]+1:1
-       }
-       for(let one in a){
-           if(a[one]== 1) 
-           return one
-       }
+       //哈希表小朋友才用
+       // let a ={}
+       // for(let i = 0;i<nums.length;i++){
+       //     a[nums[i]] = a[nums[i]]?a[nums[i]]+1:1
+       // }
+       // for(let one in a){
+       //     if(a[one]== 1) 
+       //     return one
+       // }
+       
+       //xor 這到底是什麼古老的邏輯
+        let n = nums[0]
+        for(let i =1;i<nums.length;i++){
+            n ^= nums[i]
+        }
+        return n
       };
