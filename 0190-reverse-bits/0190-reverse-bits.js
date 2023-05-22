03902205bbd46fcd0a52553fe7c1936672911bcc
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-22 11:58:58 +0800

    Time: 70 ms (41.10%), Space: 44.2 MB (38.20%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+function reverseBits(n) {
+    let res = 0
+    for(let i =0;i<32;i++){
+        res = (res<<1)|(n&1)
+        n>>=1
+    }
+    return res>>>0
+}
