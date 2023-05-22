5a63a4cbd79f8cd5ef3be82fc20c43d88568fbab
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-22 13:49:43 +0800

    Time: 70 ms (94.88%), Space: 50.1 MB (70.18%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,14 +3,19 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let res = nums[0]
-    let sum =0
-    for(let num of nums){
+    let res = nums[0] //存答案預設陣列[0]
+    let sum =0 //計算當前數組大小
+    for(let i =0;i<nums.length;i++){
+        //如果當前還是正的累加下一個數組
         if(sum>0){
-            sum +=num
+            
+            sum += nums[i]
+            
         }else{
-            sum = num
+            //否則=這個數組
+            sum = nums[i]
         }
+        //每次跑完判斷當前數組跟預設數組大小 並更新為較大的
         res = Math.max(res,sum)
     }
     return res
