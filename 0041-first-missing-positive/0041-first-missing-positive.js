fed1159193b6a05428381190b81563880765e994
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-23 15:34:03 +0800

    Time: 85 ms (63.17%), Space: 49.5 MB (72.18%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -6,6 +6,7 @@ function firstMissingPositive(nums) {
   const n = nums.length;
   // 将正整数 k 放到下标为 k-1 的位置上
   for (let i = 0; i < n; i++) {
+      
     while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
       [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
     }
