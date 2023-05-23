db6e47a0700c9778abdff50be200969af154c4dc
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-23 16:20:39 +0800

    Time: 59 ms (90.86%), Space: 44.9 MB (43.15%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.js
+++ 0665-non-decreasing-array/0665-non-decreasing-array.js
@@ -2,17 +2,20 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-function checkPossibility(nums) {
-  let count = 0;
-  for (let i = 1; i < nums.length && count <= 1; i++) {
-    if (nums[i] < nums[i - 1]) {
-      count++;
-      if (i - 2 < 0 || nums[i - 2] <= nums[i]) {
-        nums[i - 1] = nums[i];
-      } else {
-        nums[i] = nums[i - 1];
-      }
+
+
+var checkPossibility = function(nums) {
+    
+    let c = 0;
+    for (let i = 1; i < nums.length && c <= 1; i++) {
+        if (nums[i - 1] > nums[i]) {
+            c++;
+            if (i - 2 < 0 || nums[i - 2] <= nums[i]) {
+                nums[i - 1] = nums[i];
+            } else {
+                nums[i] = nums[i - 1];
+            }
+        }
     }
-  }
-  return count <= 1;
-}
+    return c <= 1;
+};
