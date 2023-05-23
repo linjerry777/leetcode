f6542a3ea85e84f7a6f13019d20dce403d383f24
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-23 14:09:56 +0800

    Time: 81 ms (57.14%), Space: 50 MB (27.82%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -3,11 +3,14 @@
  * @return {number[]}
  */
 var findErrorNums = function(nums) {
-    let n = nums.length
-    let t = (n*(n+1))/2
-    let r=-1
+    let n = nums.length //數組長
+    let t = (n*(n+1))/2 //理應的和
+    let r=-1 //重複的
     let set = new Set()
     for(let i = 0;i<nums.length;i++){
+        
+        //如果set有這個值了 代表重複r=重複
+        //否則存進set 裡面 並且總和-去該值
         if(set.has(nums[i])){
             r = nums[i]
         }else{
