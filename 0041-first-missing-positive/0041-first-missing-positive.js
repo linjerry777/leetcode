07addc3af69014111dc7d1eea21d47039dbabe7d
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-23 15:31:59 +0800

    Time: 81 ms (70.88%), Space: 47.9 MB (81.20%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -2,25 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function(nums) {
-    //哈希表
-    
-    let hash ={}
-    let len = nums.length
-    for(let i =0;i<nums.length;i++){
-        if(nums[i]>0){
-            hash[nums[i]] = 1
-        }
+function firstMissingPositive(nums) {
+  const n = nums.length;
+  // 将正整数 k 放到下标为 k-1 的位置上
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
     }
-    for(let i =1;i<=nums.length;i++){
-        if(!hash[i]){
-            return i
-        }
+  }
+  // 找到第一个不在正确位置上的正整数
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
     }
-    
-    
-    
-    
-    
-    return len+1
-};
+  }
+  // 如果数组中所有正整数都在正确位置上，则缺失的第一个正整数为数组长度加一
+  return n + 1;
+}
