3aec096b37d809f020bcc0c162c65f7287611d35
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-23 13:20:42 +0800

    Time: 61 ms (72.54%), Space: 42.3 MB (89.45%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -3,11 +3,26 @@
  * @return {number}
  */
 var thirdMax = function(nums) {
-    let set = [...new Set(nums)].sort((a,b)=>b-a)
-    if(set.length>=3){
-        return set[2]
-    }else{
-        return set[0]
+    // let set = [...new Set(nums)].sort((a,b)=>b-a)
+    // if(set.length>=3){
+    //     return set[2]
+    // }else{
+    //     return set[0]
+    // }
+    
+    let a = -Infinity,b = -Infinity,c = -Infinity
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]>a){
+            c=b
+            b=a
+            a=nums[i]
+        }else if(nums[i]<a&&nums[i]>b){
+            c=b
+            b=nums[i]
+        }else if(nums[i]<b&&nums[i]>c){
+            c=nums[i]
+        }
     }
+    return c ===-Infinity?a:c
     
 };
