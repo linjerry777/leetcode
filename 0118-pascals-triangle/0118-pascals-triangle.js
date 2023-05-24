740b08876c7b840dc9c0b69fd54b3c7375309f48
linjerry777 <117623922+linjerry777@users.noreply.github.com>
2023-05-24 08:58:40 +0800

    Time: 66 ms (24.85%), Space: 42.3 MB (58.40%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,15 +3,13 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    const ret = [];
-
-    for (let i = 0; i < numRows; i++) {
-        const row = new Array(i + 1).fill(1);
-        for (let j = 1; j < row.length - 1; j++) {
-            row[j] = ret[i - 1][j - 1] + ret[i - 1][j];
+    let res =[]
+    for(let i =0;i<numRows;i++){
+        const row = new Array(i+1).fill(1)
+        for(let j = 1;j<row.length-1;j++){
+            row[j] = res[i-1][j-1] + res[i-1][j]
         }
-        ret.push(row);
+        res.push(row)
     }
-    return ret;
+    return res
 };
-
